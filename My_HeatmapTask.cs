6e2dff86572755a669ffe46ebec7ba4d4c1fb6c9
Mary Shiposha <rusya7891@mail.ru>
2020-05-30 01:09:14 +0300

    Добавьте файлы проекта.

---
+++ My_HeatmapTask.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Names
+{
+    internal static class HeatmapTask
+    {
+        public static HeatmapData GetBirthsPerDateHeatmap(NameData[] names)
+        {
+            var birthDays = new string[30];
+            var birthMonths = new string[12];
+            var points = new double[30, 12];
+            for (var d = 0; d < birthDays.Length; d++) birthDays[d] = (d + 2).ToString();
+            for (var m = 0; m < birthMonths.Length; m++) birthMonths[m] = (m + 1).ToString();
+            foreach (var x in names)
+            {
+                if (x.BirthDate.Day != 1)
+                {
+                    points[x.BirthDate.Day - 2, x.BirthDate.Month - 1]++;
+                }
+            }
+            return new HeatmapData(
+                "Пример карты интенсивностей",
+                points,
+                birthDays,
+                birthMonths);
+        }
+    }
+}
---
+++ My_HistogramTask.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace Names
+{
+    internal static class HistogramTask
+    {
+        public static HistogramData GetBirthsPerDayHistogram(NameData[] names, string name)
+        {
+            var birthsDays = new double[31];
+            var pointDays = new string[31];
+            for (var y = 0; y < pointDays.Length; y++)
+                pointDays[y] = (y + 1).ToString();
+            foreach (var x in names)
+            {
+                if (x.Name == name && x.BirthDate.Day != 1)
+                {
+                    birthsDays[x.BirthDate.Day - 1]++;
+                }
+            }
+            return new HistogramData(string.Format("Рождаемость людей с именем '{0}'", name), pointDays, birthsDays);
+        }
+    }
+}
